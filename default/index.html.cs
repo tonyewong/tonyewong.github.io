b0db824fdf06d893a16568fb262ae933ccfd1961
Tony Wong <twong@psu.edu>
2018-06-19 14:50:46 -0600

    trying johns idea 1

---
+++ default/index.html.cs
@@ -0,0 +1,93 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" itemscope="" itemtype="../../../schema.org/WebPage.html">
+
+<style>
+    * {
+      font-family: arial;
+    }
+</style>
+
+<!-- Mirrored from sites.google.com/site/tewongwebsite/ by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 04 Sep 2017 23:57:45 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="chrome=1" />
+  <link rel="shortcut icon" type="image/x-icon" href="../../../www.google.com/images/icons/product/sites-16.ico" />
+  <!-- <img src="./pictures/tony02.jpeg" width=200> -->
+  <img src="./pictures/full_header_bar.png" width=1000>
+  <style type="text/css">
+  </style>
+  <link rel="stylesheet" type="text/css" href="../../../ssl.gstatic.com/sites/p/04fe5f/system/app/themes/simple/standard-css-simple-ltr-ltr.css" />
+  <link rel="stylesheet" type="text/css" href="_/rsrc/1503955788000/system/app/css/overlayb79c.css?cb=simple%25150goog-ws-nosidenone30themedefaultstandard" />
+  <link rel="stylesheet" type="text/css" href="_/rsrc/1503955788000/system/app/css/camelot/allthemes-view.css" />
+  <title>Tony Wong</title>
+  <meta itemprop="image" content="/site/tewongwebsite/_/rsrc/1469320910097/config/customLogo.gif?revision=8" />
+  <meta property="og:image" content="/site/tewongwebsite/_/rsrc/1469320910097/config/customLogo.gif?revision=8" />
+</head>
+
+<table id="sites-chrome-header" class="sites-layout-hbox" cellspacing="15" style="height:auto;">
+<tr class="sites-header-primary-row" id="sites-chrome-userheader">
+<td id="sites-header-title" class="" role="banner"><div class="sites-header-cell-buffer-wrapper"><a href="index.html" id="sites-chrome-userheader-logo"></a><h2><a href="index.html" dir="ltr" id="sites-chrome-userheader-title">Tony Wong</a></h2></div></td>
+</tr>
+</table>
+
+<div id="main-wrapper">
+
+<table id="main" class="sites-layout-hbox" cellspacing="15" cellpadding="{scmCellpadding}" border="0">
+
+<tr>
+
+<td id="sites-chrome-sidebar-left" class="sites-layout-sidebar-left" style="display: none; width: 150px">
+<div xmlns="http://www.w3.org/1999/xhtml" id="COMP_4333703097350603" class="sites-embed" role="navigation"><div class="sites-embed-content sites-sidebar-nav"><ul role="navigation" jotId="navList"><li class="nav-first "><div dir="ltr" style="padding-left: 5px;"><a href="home/teaching.html" jotId="wuid:gx:3440829c7d3e2ca2" class="sites-navigation-link">Teaching</a></div></li><li class=""><div dir="ltr" style="padding-left: 5px;"><a href="home/research.html" jotId="wuid:gx:60a99b02797bdbd7" class="sites-navigation-link">Research</a></div></li><li class=""><div dir="ltr" style="padding-left: 5px;"><a href="home/publications.html" jotId="wuid:gx:5a0fd94ae86fe5b4" class="sites-navigation-link">Publications</a></div></li><li class=""><div dir="ltr" style="padding-left: 5px;"><a href="home/selected-presentations.html" jotId="wuid:gx:63f10d6fd7c960b2" class="sites-navigation-link">Selected presentations</a></div></li><li class=""><div dir="ltr" style="padding-left: 5px;"><a href="../../../drive.google.com/file/d/0BzfIQ3K44Vk4YzdpNnE0WFk1b3c/viewce8c.html?id=0BzfIQ3K44Vk4YzdpNnE0WFk1b3c" class="sites-navigation-link" target="_blank">CV</a></div></li></ul></div></div>
+</td>
+<td id="sites-canvas-wrapper">
+<div id="sites-canvas" role="main">
+
+<table xmlns="http://www.w3.org/1999/xhtml" cellspacing="0" class="sites-layout-name-one-column sites-layout-hbox"><tbody><tr><td class="sites-layout-tile sites-tile-name-content-1"><div dir="ltr"><div dir="ltr"><span style="font-size:13.3333px;line-height:16.6667px"><font face="arial, sans-serif"><div><div><a href="home/teaching.html">teaching</a>    |    <a href="home/research.html">research</a><span>    |     <a href="home/publications.html">publications</a>    |<span>    </span><a href="home/selected-presentations.html">selected presentations</a><span>    |    <a href="https://drive.google.com/open?id=0BzfIQ3K44Vk4YzdpNnE0WFk1b3c">current(ish) CV</a></span></span></div><div><div></div></div></div>
+
+<div dir="ltr"><br /></div>
+<div dir="ltr"><hr /></div>
+<div dir="ltr"><br /></div>
+<div dir="ltr">Mathematics is a powerful tool which enables us to answer interesting questions. In my research, I use mathematical and statistical tools to characterize uncertainty in physical models. In the classroom, I aim to draw on these experiences to show students that mathematics and computational science are much more than just a set of prerequisites for their other coursework.</div>
+
+<h3>Current work:</h3>
+  <div>Instructor, Computer Science</div>
+  <div>University of Colorado Boulder</div>
+  <div><br /></div>
+
+<h3>Research interests:</h3>
+  <div>Uncertainty in climate model projections, sea level rise in particular, can lead to suboptimal, ineffective, and - at worst - outright dangerous policy decisions. To avoid this, we must use the information we have available make the best possible policy decisions. This requires accounting for not only varying forms of uncertainty in model parameters and projections, but deep uncertainty - uncertainty <b>in the uncertainty</b> in model structure and parameters. Statistical calibration approaches allow us to constrain these models and characterize the uncertainties inherent in both the model and data, and are a critical part of any modeling effort.</div>
+  <div>In particular, I am interested in future projections of sea-level rise and their impacts on coastal defense decision-making.</div>
+
+  <div><br /></div>
+  <a href="https://www.researchgate.net/profile/Tony_Wong19">ResearchGate</a>
+  <div></div>
+  <a href="https://goo.gl/Hu21nB">Google Scholar</a>
+  <div><br /></div>
+
+<h3>Education:</h3>
+  <div>PhD, Applied Mathematics, University of Colorado at Boulder, 2016</div>
+  <div>MS, Applied Mathematics, University of Colorado at Boulder, 2012</div>
+  <div>BA, Mathematics (hons.) and Astrophysics, Ohio Wesleyan University, 2010</div>
+
+<h3><a name="TOC-Contact-information:"></a>Contact information:</h3>
+  <div>Office: ECOT 623</div>
+  <div>Email: anthony.e.wong@colorado.edu</div>
+  <div>Department of Computer Science</div>
+  <div>430 UCB</div>
+  <div>University of Colorado Boulder</div>
+  <div>Boulder, CO 80309-0430, USA</div>
+
+<div dir="ltr"><br /></div>
+<div dir="ltr"><hr /></div>
+<div dir="ltr"><br /></div>
+
+</div> 
+</td> 
+</tr>
+</table> 
+
+</div> 
+</div> 
+</body>
+</html>
